785b587848fe80a828a9ee37f9d00372e7ef0dc8
jdquimbiulco <juandiegoq21@gmail.com>
2023-02-09 23:37:14 -0500

    Using sout and primitive data types with java

---
+++ JavaPrimerosPasos/src/TipoVariable.java
@@ -0,0 +1,20 @@
+
+public class TipoVariable {
+	
+	public static void main(String[] args){
+		System.out.println("Hola Mundo! Me llamo Diego");
+		
+		int edad = 22;
+		System.out.println("Mi edad actual es: " + edad);
+		
+		double salario;
+		double salarioEnUnAnho;
+		
+		salario = 450.00;
+		salarioEnUnAnho = salario * 12;
+		
+		System.out.println("Mi salario basico es: " + salario);
+		System.out.println("Mi salario anual es: " + salarioEnUnAnho);
+	}
+	
+}
