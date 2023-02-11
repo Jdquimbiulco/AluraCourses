3d39fd57c10336e21e3084f4e5abe7fb5eba90eb
jdquimbiulco <juandiegoq21@gmail.com>
2023-02-11 00:12:29 -0500

    Converting valriable types with the casting method

--- JavaPrimerosPasos/src/TipoVariable.java
+++ JavaPrimerosPasos/src/TipoVariable.java
@@ -4,17 +4,22 @@ public class TipoVariable {
 	public static void main(String[] args){
 		System.out.println("Hola Mundo! Me llamo Diego");
 		
+		
 		int edad = 22;
+		double salario = 450.000;
+		double salarioEnUnAnho = salario * 12;
+		double division = 5.0/2;
+		int transformacionDeTipo = (int) salario; //"Cast" method transforms type of data.
+		float valor1 = 0.1F;
+		float valor2 = 0.2F;
+		float total = valor1 + valor2;
+		
 		System.out.println("Mi edad actual es: " + edad);
-		
-		double salario;
-		double salarioEnUnAnho;
-		
-		salario = 450.00;
-		salarioEnUnAnho = salario * 12;
-		
 		System.out.println("Mi salario basico es: " + salario);
 		System.out.println("Mi salario anual es: " + salarioEnUnAnho);
+		System.out.println("El resultado de la division es: " + division);
+		System.out.println("Mi salario redondeado es: " + transformacionDeTipo);
+		System.out.println("El total de la suma es: " + total);
 	}
 	
 }
