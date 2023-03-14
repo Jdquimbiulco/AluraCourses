41910bd76fdbae538e6faa8e256155bb6921e15b
jdquimbiulco <juandiegoq21@gmail.com>
2023-03-13 23:32:53 -0500

    Creating new exceptions and using throw to test if it works

--- JavaPilaEjecucion/src/Flujo.java
+++ JavaPilaEjecucion/src/Flujo.java
@@ -8,35 +8,22 @@ public class Flujo {
 	}
 
 	public static void metodo1() {
-		System.out.println("Inicio de metodo 1");
-		metodo2();
+		System.out.println("Inicio de metodo 1");	
+		try {
+			metodo2();
+		} catch (MiException excep) {
+			excep.getMessage();
+			excep.printStackTrace();
+		}
+		
 		System.out.println("Final de metodo 1");
 	}
 
+
 	public static void metodo2() {
 		System.out.println("Inicio de metodo 2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
+		//throw new ArithmeticException(); //Lanza un nueva excepcion del tipo arithmetic
+		throw new MiException("Mi excepcion fue lanzada");
 
-			try { //Intenta esto
-//				int num = 0;
-//				int resultado = i / num;
-//				System.out.println(resultado);
-				
-				String test = null;
-				System.out.println(test.toString());
-				
-			} catch(ArithmeticException | NullPointerException exception) { //atrapa el error
-				
-				System.out.println("ATRAPO EXCEPCION");
-				System.out.println(exception.getMessage());
-				exception.printStackTrace();
-				
-			}
-			
-
-
-		}
-		System.out.println("Final de metodo 2");
 	}
 }
---
+++ JavaPilaEjecucion/src/Flujo2.java
@@ -0,0 +1,59 @@
+
+public class Flujo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Inicio de Main");
+		metodo1();
+		System.out.println("Final de Main");
+	}
+
+	public static void metodo1() {
+		System.out.println("Inicio de metodo 1");
+		
+//		try {  //prueba con una clase creada 
+//				Cuenta cuentaNueva = null;
+//				cuentaNueva.depositar();
+//			
+//		} 
+//			catch(NullPointerException excep){
+//			String msg = excep.getMessage();
+//			System.out.println("Excepcion" + msg);
+//			excep.printStackTrace();
+//		}
+		
+		metodo2();
+		System.out.println("Final de metodo 1");
+	}
+
+
+	public static void metodo2() {
+		System.out.println("Inicio de metodo 2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+
+			try { //Intenta esto
+				
+				if (i==3) {
+					int num = 0;
+					int resultado = i / num;
+					System.out.println(resultado);
+					
+				}			
+				
+				String test = null;
+				System.out.println(test.toString());
+				
+			} catch(ArithmeticException | NullPointerException exception) { //atrapa el error
+				
+				System.out.println("ATRAPO EXCEPCION");
+				System.out.println(exception.getMessage());
+				exception.printStackTrace();
+				
+			}
+			
+
+
+		}
+		System.out.println("Final de metodo 2");
+	}
+}
