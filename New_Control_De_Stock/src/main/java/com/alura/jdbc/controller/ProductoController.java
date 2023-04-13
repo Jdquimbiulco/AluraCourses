c7e22782ed80d9f3610c97e41bb32274c2d63883
jdquimbiulco <juandiegoq21@gmail.com>
2023-04-12 23:42:46 -0500

    created a connection pool with c3p0

--- New_Control_De_Stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ New_Control_De_Stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -12,151 +12,162 @@ import java.util.Map;
 
 import com.alura.jdbc.factory.ConnectionFactory;
 
-
 public class ProductoController {
 
 	public int modificar(String nombre, String descripcion, Integer id, Integer cantidad) throws SQLException {
-		
-		Connection connection = new ConnectionFactory().recuperaConexion();
-		
+
+		final Connection connection = new ConnectionFactory().recuperaConexion();
+
+		try (connection) {
 //		Statement statement = connection.createStatement();
 //		statement.execute("UPDATE PRODUCTO SET "
 //				+ " NOMBRE = '" + nombre + "'"
 //				+ ", DESCRIPCION = '"  + descripcion + "'"
 //				+ ", CANTIDAD = " + cantidad
 //				+ " WHERE ID = " + id);
-		
-		PreparedStatement statement = connection.prepareStatement("UPDATE PRODUCTO SET "
-				+ " NOMBRE = ? "
-				+ ", DESCRIPCION = ? "
-				+ ", CANTIDAD = ? " 
-				+ " WHERE ID = ? ");
-		
-		statement.setString(1, nombre);
-		statement.setString(2, descripcion);
-		statement.setInt(3, cantidad);
-		statement.setInt(4, id);
-		
-		statement.execute();
-		
-		
-		
-		int updateCount = statement.getUpdateCount();
-		
-		connection.close();
-		
-		return updateCount;
-		
+
+			final PreparedStatement statement = connection.prepareStatement("UPDATE PRODUCTO SET " + " NOMBRE = ? "
+					+ ", DESCRIPCION = ? " + ", CANTIDAD = ? " + " WHERE ID = ? ");
+
+			try (statement) {
+				statement.setString(1, nombre);
+				statement.setString(2, descripcion);
+				statement.setInt(3, cantidad);
+				statement.setInt(4, id);
+
+				statement.execute();
+
+				int updateCount = statement.getUpdateCount();
+
+				return updateCount;
+
+			}
+		}
+
 	}
 
 	public int eliminar(Integer id) throws SQLException {
-		Connection connection = new ConnectionFactory().recuperaConexion();
+
+		final Connection connection = new ConnectionFactory().recuperaConexion();
 //OLD VERSION VULNERBLE TO SQL INJECTIONS		
 //		Statement statement = connection.createStatement();
 //		statement.execute("DELETE FROM PRODUCTO WHERE ID = " + id);
-		
-		PreparedStatement statement = connection.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
-		statement.setInt(1, id);
-		statement.execute();
-		connection.close();
-		return statement.getUpdateCount();
-		
-		
+
+		try (connection) {
+
+			final PreparedStatement statement = connection.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
+
+			try (statement) {
+				statement.setInt(1, id);
+				statement.execute();
+
+				return statement.getUpdateCount();
+			}
+
+		}
+
 	}
 
 	public List<Map<String, String>> listar() throws SQLException {
-		
-		Connection connection = new ConnectionFactory().recuperaConexion();
-		
-//OLD VERSION VULNERBLE TO SQL INJECTIONS		
-//		Statement statement = connection.createStatement();
-//		
-//		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
-		
-		PreparedStatement statement = connection.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
-		statement.execute();
-		
-		
-		
-		ResultSet resultSet = statement.getResultSet();
-		
-		List<Map<String, String>> resultado = new ArrayList<>();
-		
-		while(resultSet.next()) {
-			
-			Map<String, String> fila = new HashMap<>();
-			fila.put("ID",String.valueOf(resultSet.getInt("ID")));
-			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
-			fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
-			fila.put("CANTIDAD",String.valueOf(resultSet.getInt("CANTIDAD")));
-			
-			resultado.add(fila);
+
+		final Connection connection = new ConnectionFactory().recuperaConexion();
+
+		try (connection) {
+
+			// OLD VERSION VULNERBLE TO SQL INJECTIONS
+			// Statement statement = connection.createStatement();
+			//
+			// statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+			final PreparedStatement statement = connection
+					.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+			try (statement) {
+				statement.execute();
+
+				ResultSet resultSet = statement.getResultSet();
+
+				List<Map<String, String>> resultado = new ArrayList<>();
+
+				while (resultSet.next()) {
+
+					Map<String, String> fila = new HashMap<>();
+					fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+					fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+					fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+					fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+
+					resultado.add(fila);
+				}
+
+				return resultado;
+			}
 		}
-		
-		connection.close();
-		
-		return resultado;
 	}
 
-    public void guardar(Map<String, String> producto) throws SQLException {
+	public void guardar(Map<String, String> producto) throws SQLException {
 		String nombre = producto.get("NOMBRE");
 		String descripcion = producto.get("DESCRIPCION");
 		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
 		Integer maximoCantidad = 50;
-    	
-    	Connection connection = new ConnectionFactory().recuperaConexion();
-    	connection.setAutoCommit(false);
-    	
-		
-// OLD VERSION VULNERBLE TO SQL INJECTIONS
-//		Statement statement = connection.createStatement();
-//		
-//		statement.execute("INSERT INTO PRODUCTO(nombre, descripcion, cantidad) "
-//				+ " VALUES('" + producto.get("NOMBRE") + "', '"
-//				+ producto.get("DESCRIPCION") + "', "
-//				+ producto.get("CANTIDAD") + ")", statement.RETURN_GENERATED_KEYS);
-		
-		PreparedStatement statement = connection.prepareStatement("INSERT INTO PRODUCTO(nombre, descripcion, cantidad)"
-				+ "VALUES (? , ? , ?)",
-				Statement.RETURN_GENERATED_KEYS);
-		
-		try { 
-		
-		do {
-			int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
-			
-			ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
-			
-			cantidad -= maximoCantidad;
-		}while(cantidad > 0);	
-		
-		connection.commit();
-		
-		} catch(Exception e) {
-			connection.rollback();
+
+		final Connection connection = new ConnectionFactory().recuperaConexion();
+
+		try (connection) {
+			connection.setAutoCommit(false);
+
+			// OLD VERSION VULNERBLE TO SQL INJECTIONS
+			// Statement statement = connection.createStatement();
+			//
+			// statement.execute("INSERT INTO PRODUCTO(nombre, descripcion, cantidad) "
+			// + " VALUES('" + producto.get("NOMBRE") + "', '"
+			// + producto.get("DESCRIPCION") + "', "
+			// + producto.get("CANTIDAD") + ")", statement.RETURN_GENERATED_KEYS);
+
+			final PreparedStatement statement = connection.prepareStatement(
+					"INSERT INTO PRODUCTO(nombre, descripcion, cantidad)" + "VALUES (? , ? , ?)",
+					Statement.RETURN_GENERATED_KEYS);
+
+			try (statement) {
+
+				do {
+					int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
+
+					ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+
+					cantidad -= maximoCantidad;
+				} while (cantidad > 0);
+
+				connection.commit();
+				System.out.println("COMMIT");
+
+			} catch (Exception e) {
+				connection.rollback();
+				System.out.println("ROLLBACK");
+			}
+
 		}
-		
-		statement.close();
-		connection.close();
+
 	}
 
 	private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
 			throws SQLException {
 		statement.setString(1, nombre);
-		statement.setString(2, descripcion);		
+		statement.setString(2, descripcion);
 		statement.setInt(3, cantidad);
-		
+
 		statement.execute();
-		
-		ResultSet resultSet = 	statement.getGeneratedKeys();
-		
-		while(resultSet.next()) {
-		
-			System.out.println(
-					String.format(
-							"Fue insertado el producto de ID %d", 
-							resultSet.getInt(1)));
+
+		final ResultSet resultSet = statement.getGeneratedKeys();
+
+		try (resultSet) {
+
+			while (resultSet.next()) {
+
+				System.out.println(String.format("Fue insertado el producto de ID %d", resultSet.getInt(1)));
+			}
 		}
+
 	}
 
 }
--- New_Control_De_Stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ New_Control_De_Stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -4,14 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
 	
-	public Connection recuperaConexion() throws SQLException {
-		return DriverManager.getConnection(
-		"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
-		"root",
-		"SQL1553");
-			
+	private  DataSource dataSource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("SQL1553");	
+		
+		this.dataSource = pooledDataSource;
+	
 	}
+	
+	public Connection recuperaConexion() throws SQLException {
+	return this.dataSource.getConnection();	
+}
+	
+//OLD VERSION PURE CONNECTION METHOD WITH OUT POOL 	
+//	public Connection recuperaConexion() throws SQLException {
+//		return DriverManager.getConnection(
+//		"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+//		"root",
+//		"SQL1553");
+//			
+//	}
 
 }
